62855d37dc8151c6c9da39b6dcb5248a9108a324
Jack <jack.timothy.rust@gmail.com>
2020-06-20 17:45:14 +1000

    Test Google cloud build
    If working I should see V2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Official website of Jack Rust.
+          Official website of Jack Rust - V2.
         </p>
         <a
           className="App-link"
