9ef7c410f867262423f155d1829549eb846050cb
Jack <jack.timothy.rust@gmail.com>
2020-06-20 14:01:56 +1000

    Deploying to Google Cloud

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Official website of Jack Rust.
+        </p>
+        <a
+          className="App-link"
+          href="https://twitter.com/JackTimothyRust"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
