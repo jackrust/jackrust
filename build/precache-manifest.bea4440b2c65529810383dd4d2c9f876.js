b73aacc551f9778b157e9ae43abdd3a6427adb8d
Jack <jack.timothy.rust@gmail.com>
2020-06-21 12:24:21 +1000

    Including build folder
     to test static website serve

---
+++ build/precache-manifest.bea4440b2c65529810383dd4d2c9f876.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c0508dc1303afaf544295ac493e74a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a00313fa7c21e15fcae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2a921adb50cfa101f94",
+    "url": "/static/js/2.222e0358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.222e0358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a00313fa7c21e15fcae",
+    "url": "/static/js/main.555d950f.chunk.js"
+  },
+  {
+    "revision": "87e8f3e2a47610b86ffb",
+    "url": "/static/js/runtime-main.49a1c0af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
