fdee954e098121b278b4c478dfdf6af5d17553a9
Jack <jack.timothy.rust@gmail.com>
2020-06-21 16:47:20 +1000

    updating yaml to deploy to the app engine

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Official website of Jack Rust - V8
+          Official website of Jack Rust - V1.01
         </p>
         <a
           className="App-link"
