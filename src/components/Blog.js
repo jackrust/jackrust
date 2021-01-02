7320a339815719ca2574debdd14751940b4eab22
Jack <jack.timothy.rust@gmail.com>
2021-01-02 12:31:41 +1100

    Adds missed files

---
+++ src/components/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+       <div>
+          <h1>Blog</h1>
+          <p>Pending...</p>
+       </div>
+    );
+}
+
+export default Blog;
+
+//ReactDOM.render(<Menu />, document.getElementById('menu'));
